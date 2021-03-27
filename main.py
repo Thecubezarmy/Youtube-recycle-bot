e020593a2432debf6ffa3895d38a371c662822be
Thecubezarmy <81333422+Thecubezarmy@users.noreply.github.com>
2021-03-27 13:54:01 +0200

    Update main.py
    felt cute, changed the file system
    might delete later

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from moviepy.editor import *
 import moviepy.editor as mp
 import re
 from os import listdir
+import moviepy.editor
 
 
-tgt_folder = "\youtube-recycle-bot\mp3's"
+tgt_folder = "\youtube-recycle-bot\output"
 
 print ("enter username")
 username = input()
@@ -27,14 +28,19 @@ for x in listofsongs:
     z=queryfunction.youtubequery(x)
     youtube_link = z
     w = YouTube(youtube_link).streams.first()
-    w.download(output_path="\youtube-recycle-bot\mp3's")
+    w.download(output_path="\youtube-recycle-bot\output")
 
-for file in [n for n in os.listdir(tgt_folder) if re.search('mp4',n)]:
- full_path = os.path.join(tgt_folder, file)
- output_path = os.path.join(tgt_folder, os.path.splitext(file)[0] + '.mp3')
- clip = mp.AudioFileClip(full_path)
- clip.write_audiofile(output_path)
 
+
+#converts the mp4 to mp3
+#for file in [n for n in os.listdir(tgt_folder) if re.search('mp4',n)]:
+ #full_path = os.path.join(tgt_folder, file)
+ #output_path = os.path.join(tgt_folder, os.path.splitext(file)[0] + '.mp3')
+ #clip = mp.AudioFileClip(full_path)
+ #clip.write_audiofile(output_path)
+
+filecounter = 0
 for file_name in listdir(tgt_folder):
-   if file_name.endswith('.mp4'):
-     os.remove(tgt_folder+ "\\" + file_name)
+ os.rename(tgt_folder + "\\"+ file_name,tgt_folder + "\\"+"Video"+str(filecounter)+".mp4")
+ filecounter = filecounter + 1
+
